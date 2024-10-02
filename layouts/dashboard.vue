49181b9b76a66c67d4304ac69e51c0200cf7e413
Asher Lien <asher.lien@riversoft.com.tw>
2024-10-02 16:21:15 +0800

    完成手機版

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,11 +1,14 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
-import { AdminRoutes } from '~/enums'
+import { AdminRoutes } from '@/enums'
 
 const isMenuOpen = ref(false)
 
 const toggleMenu = () => {
   isMenuOpen.value = !isMenuOpen.value
+}
+
+const closeMenu = () => {
+  isMenuOpen.value = false
 }
 
 const links = [
@@ -28,29 +31,25 @@ const links = [
         icon="ion:menu"
         size="sm"
         color="primary"
-        square
         variant="ghost"
         @click="toggleMenu"
       />
     </header>
-    <div
-      v-if="isMenuOpen"
-      class="fixed inset-0 bg-gray-800 bg-opacity-50 z-10"
-      @click="toggleMenu"
-    />
-    <nav
-      class="fixed top-0 right-0 h-full bg-white transform transition-transform z-20"
-      :class="[isMenuOpen ? 'translate-x-0 w-9/12' : 'translate-x-full']"
-    >
-      <UVerticalNavigation :links="links">
-        <template #default="{ link }">
-          <span class="group-hover:text-primary relative">{{ link.label }}</span>
-        </template>
-      </UVerticalNavigation>
-    </nav>
+    <UModal v-model="isMenuOpen">
+      <div class="p-4">
+        <UVerticalNavigation
+          :links="links"
+          @click="closeMenu"
+        >
+          <template #default="{ link }">
+            <span class="group-hover:text-primary relative">{{ link.label }}</span>
+          </template>
+        </UVerticalNavigation>
+        <Placeholder class="h-48" />
+      </div>
+    </UModal>
     <UContainer>
       <slot />
     </UContainer>
-    <header>Footer</header>
   </div>
 </template>
