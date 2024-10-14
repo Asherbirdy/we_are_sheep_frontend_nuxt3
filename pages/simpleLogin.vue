e67831177f539147b179c1bcfcae422e4da75f9f
Asher Lien <asher.lien@riversoft.com.tw>
2024-10-14 14:53:48 +0800

    簡易切版

---
+++ pages/simpleLogin.vue
@@ -0,0 +1,37 @@
+<script setup lang='ts'>
+const state = ref({
+  email: '',
+  password: '',
+})
+
+const handleSubmit = () => {
+  console.log(state.value)
+}
+</script>
+
+<template>
+  <div>
+    <p>email</p>
+    <input
+      v-model="state.email"
+      class="border border-gray-300 p-2 rounded-md"
+      type="text"
+    >
+    <p>password</p>
+    <input
+      v-model="state.password"
+      class="border border-gray-300 p-2 rounded-md"
+      type="password"
+    >
+    <button
+      class="bg-blue-500 text-white p-2 rounded-md"
+      @click="handleSubmit"
+    >
+      login
+    </button>
+  </div>
+</template>
+
+<style>
+
+</style>
