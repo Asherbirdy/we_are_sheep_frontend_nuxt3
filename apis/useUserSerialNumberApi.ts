eec49c3b3c04ac2f2fb9858004292c15f51b7c3d
Asher Lien <asher.lien@riversoft.com.tw>
2024-10-21 11:48:38 +0800

    getAll api 及 type

--- apis/useUserSerialNumberApi.ts
+++ apis/useUserSerialNumberApi.ts
@@ -1,11 +1,17 @@
 import type { UserSerialNumberResponse } from '@/types'
+import { useNuxtApp } from '#app'
 
+const nuxtApp = useNuxtApp()
 export const useUserSerialNumberApi = {
   getAll: async () => {
     return await useRequestApi<UserSerialNumberResponse>('/userSerialNumber/getAll', {
       method: 'GET',
       server: false,
       lazy: false,
+      key: '/userSerialNumber/getAll',
+      getCachedData(key) {
+        return nuxtApp.payload.data[key] || nuxtApp.static.data[key]
+      },
     })
   },
   // create: async (payload: any) => {
