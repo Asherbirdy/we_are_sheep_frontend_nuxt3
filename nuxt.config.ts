c9bc732d3d3176a2813235c8f0640daf53bff238
Asher Lien <asher.lien@riversoft.com.tw>
2024-10-21 14:57:30 +0800

    更新

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
   ssr: true,
   routeRules: {
     '/admin/member/serial': { ssr: false },
+    'admin/member/district': { ssr: false },
   },
   devtools: { enabled: true },
   compatibilityDate: '2024-10-02',
