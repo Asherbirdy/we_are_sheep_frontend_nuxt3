aef9cb4a464c698e5b6dc65726ef4516f860f741
Asher Lien <asher.lien@riversoft.com.tw>
2024-10-21 17:35:50 +0800

    切版完成

--- components/apps/member/district/EditDistrictFormComponent.vue
+++ components/apps/member/district/EditDistrictFormComponent.vue
@@ -1,22 +1,54 @@
 <script setup lang='ts'>
 import type { District } from '@/types'
+import { object, string } from 'yup'
 
 const props = defineProps<{
   data: District | null
 }>()
 
+const state = ref({
+  data: {
+    name: '',
+  },
+})
+
 const { data } = toRefs(props)
+onMounted(() => {
+  if (data.value) {
+    state.value.data.name = data.value.name
+  }
+})
+const schema = object({
+  name: string().required('請輸入區的名稱'),
+})
+const onSubmit = async () => {
+  // eslint-disable-next-line no-console
+  console.log(state.value.data)
+}
 </script>
 
 <template>
   <UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
     <template #header>
-      <div>adsa</div>
+      <div>編輯區域</div>
     </template>
-
-    <div>{{ data }}</div>
-    <template #footer>
-      <div>asdas</div>
-    </template>
+    <UForm
+      :schema="schema"
+      :state="state.data"
+      class="space-y-4"
+      @submit="onSubmit"
+    >
+      <UFormGroup
+        label="名稱"
+        name="name"
+      >
+        <UInput v-model="state.data.name" />
+      </UFormGroup>
+      <UButton
+        type="submit"
+      >
+        Submit
+      </UButton>
+    </UForm>
   </UCard>
 </template>
