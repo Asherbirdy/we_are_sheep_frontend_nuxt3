f5869f4f4da68f7685dc02128a9325c1856536f2
WILL CHOU <will92068@gmail.com>
2024-11-06 21:06:49 +0800

    11/06更新

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,136 +1,146 @@
 <script lang="ts" setup>
-import type { FormSubmitEvent } from '#ui/types'
-import { useAuthApi } from '@/apis'
-import { PublicRoutes } from '@/enums'
-import { reactive, ref } from 'vue'
-import { type InferType, object, string } from 'yup'
+import type { FormSubmitEvent } from "#ui/types";
+import { useAuthApi } from "@/apis";
+import { CookieEnums, PublicRoutes } from "@/enums";
+
+import { reactive, ref } from "vue";
+import { type InferType, object, string } from "yup";
 
 // 定義導航路由
 const routes = [
   {
-    label: '生命讀經',
+    label: "生命讀經",
     to: PublicRoutes.Home,
   },
   {
-    label: '近期活動',
+    label: "近期活動",
     to: PublicRoutes.News,
   },
   {
-    label: '會員中心',
+    label: "會員中心",
     to: PublicRoutes.Login,
   },
-]
+];
 
 // 控制彈窗開關的變數
-const isOpen = ref(false)
+const isOpen = ref(false);
 // 控制當前顯示的表單（登入或註冊）
-const isLogin = ref(true)
+const isLogin = ref(true);
 
 // 登入表單的驗證規則
 const loginSchema = object({
-  email: string().email('Invalid email').required('Required'),
+  email: string().email("Invalid email").required("Required"),
   password: string()
-    .min(8, 'Must be at least 8 characters')
-    .required('Required'),
-})
+    .min(8, "Must be at least 8 characters")
+    .required("Required"),
+});
 
 // 註冊表單的驗證規則
 const registerSchema = object({
-  email: string().email('Invalid email').required('Required'),
+  email: string().email("Invalid email").required("Required"),
   password: string()
-    .min(8, 'Must be at least 8 characters')
-    .required('Required'),
+    .min(8, "Must be at least 8 characters")
+    .required("Required"),
   confirmPassword: string()
     // .oneOf([ref('password')], 'Passwords must match')
-    .required('Required'),
-  serialNumber: string().required('Required'), // 新增序號的驗證規則
-})
+    .required("Required"),
+  serialNumber: string().required("Required"), // 新增序號的驗證規則
+});
 
 // 使用 Yup 定義的驗證規則類型
-type LoginSchema = InferType<typeof loginSchema>
-type RegisterSchema = InferType<typeof registerSchema>
+type LoginSchema = InferType<typeof loginSchema>;
+type RegisterSchema = InferType<typeof registerSchema>;
 
 // 登入表單狀態，包含 email、password 和 confirmPassword
 const LoginState = ref({
-  email: 'asherbirder@gmail.com666',
-  password: 'password',
+  email: "asherbirder@gmail.com666",
+  password: "password",
   // confirmPassword: '',
-})
+});
 // 註冊表單狀態，包含 email、password 和 confirmPassword
 const RegisterState = ref({
-  name: '',
-  email: 'asherbirder@gmail.com666',
-  password: 'password',
-  confirmPassword: 'password',
-  serialNumber: '222',
-})
+  name: "",
+  email: "asherbirder@gmail.com666",
+  password: "password",
+  confirmPassword: "password",
+  serialNumber: "222",
+});
 
 const {
+  data: loginResponse,
   // execute, // 一個可以用來刷新handler函數傳回的資料的函數。
   execute: loginExecute, // 一個可以用來刷新handler函數傳回的資料的函數。
   error: loginError,
   status: loginStatus,
-
-} = await useAuthApi.login(LoginState.value) //  會傳state form 表單的值進 useAuthApi.ts
+} = await useAuthApi.login(LoginState.value); //  會傳state form 表單的值進 useAuthApi.ts
 
 const {
   execute: registerExecute, // 一個可以用來刷新handler函數傳回的資料的函數。
   error: registerError,
   status: registerStatus,
-} = await useAuthApi.register(RegisterState.value) //  會傳state form 表單的值進 useAuthApi.ts
+} = await useAuthApi.register(RegisterState.value); //  會傳state form 表單的值進 useAuthApi.ts
 
 // 切換到登入表單
 const showLogin = () => {
-  isLogin.value = true // 顯示登入表單
-}
+  isLogin.value = true; // 顯示登入表單
+};
 
 // 切換到註冊表單
 const showRegister = () => {
-  isLogin.value = false // 顯示註冊表單
-}
+  isLogin.value = false; // 顯示註冊表單
+};
 
 // 登入表單提交處理函數
 const onSubmit = async (event: FormSubmitEvent<LoginSchema>) => {
   // 登入邏輯
-  await loginExecute()
-  console.log(LoginState.value.email)
-  console.log(LoginState.value.password)
+  await loginExecute();
+  console.log(LoginState.value.email);
+  console.log(LoginState.value.password);
 
-  console.log('LoginState', event.data)
-}
+  console.log("LoginState", event.data);
+  if (loginError.value) {
+    alert("登入錯誤");
+  }
+  const accessTokenCookie = useCookie(CookieEnums.AccessToken);
+  const refreshTokenCookie = useCookie(CookieEnums.RefreshToken);
+  if (loginResponse.value) {
+    accessTokenCookie.value = loginResponse.value.token.accessTokenJWT;
+    refreshTokenCookie.value = loginResponse.value.token.refreshTokenJWT;
+  }
+};
 
 // 註冊表單提交處理函數
 const onRegister = async (event: FormSubmitEvent<RegisterSchema>) => {
   // 註冊邏輯
-  await registerExecute()
-  console.log(RegisterState.value.email)
-  console.log(RegisterState.value.password)
-  console.log('RegisterState', event.data)
-}
+  await registerExecute();
+  console.log(RegisterState.value.email);
+  console.log(RegisterState.value.password);
+  console.log("RegisterState", event.data);
+};
 
 // 彈窗開關函數
 const Modal = () => {
-  isOpen.value = !isOpen.value // 切換彈窗顯示狀態
-  console.log('彈窗被打開')
-}
+  isOpen.value = !isOpen.value; // 切換彈窗顯示狀態
+  console.log("彈窗被打開");
+};
 
 // 切換表單的函數
 function toggleForm(linkTo: string) {
-  isLogin.value = linkTo === '#login' // 根據鏈接決定顯示哪個表單
-  isOpen.value = true // 打開彈窗
+  isLogin.value = linkTo === "#login"; // 根據鏈接決定顯示哪個表單
+  isOpen.value = true; // 打開彈窗
 }
 
 // 定義表單切換鏈接
 const links = [
   {
-    label: '登入',
-    to: '#login',
+    label: "登入",
+    to: "#login",
   },
   {
-    label: '註冊',
-    to: '#register',
+    label: "註冊",
+    to: "#register",
   },
-]
+];
 </script>
 
 <template>
@@ -143,8 +153,7 @@ const links = [
             v-for="route in routes"
             :key="route.to"
             :to="route.to"
-            class="text-gray-600"
-          >
+            class="text-gray-600">
             {{ route.label }}
           </NuxtLink>
         </nav>
@@ -154,19 +163,13 @@ const links = [
       <slot />
     </main>
     <nav
-      class="fixed bottom-0 left-0 right-0 bg-gray-100 shadow-lg none md:hidden"
-    >
+      class="fixed bottom-0 left-0 right-0 bg-gray-100 shadow-lg none md:hidden">
       <div class="grid grid-cols-3 h-16">
         <div
           v-for="route in routes"
           :key="route.to"
-          class="flex items-center justify-center border-r border-gray-200 last:border-r-0"
-        >
-          <NuxtLink
-            :to="route.to"
-            class="text-gray-600"
-            @click="Modal"
-          >
+          class="flex items-center justify-center border-r border-gray-200 last:border-r-0">
+          <NuxtLink :to="route.to" class="text-gray-600" @click="Modal">
             {{ route.label }}
           </NuxtLink>
         </div>
@@ -178,14 +181,12 @@ const links = [
         <div class="flex">
           <button
             class="base font rounded block flex-1 relative group"
-            @click="showLogin"
-          >
+            @click="showLogin">
             登入
           </button>
           <button
             class="base font rounded block flex-1 relative group"
-            @click="showRegister"
-          >
+            @click="showRegister">
             註冊
           </button>
         </div>
@@ -195,24 +196,14 @@ const links = [
             v-if="isLogin"
             :schema="loginSchema"
             :state="LoginState"
-            class="space-y-4"
-          >
-            <UFormGroup
-              label="Email"
-              name="email"
-            >
+            class="space-y-4">
+            <UFormGroup label="Email" name="email">
               <UInput v-model="LoginState.email" />
               <!-- 綁定 email -->
             </UFormGroup>
 
-            <UFormGroup
-              label="Password"
-              name="password"
-            >
-              <UInput
-                v-model="LoginState.password"
-                type="password"
-              />
+            <UFormGroup label="Password" name="password">
+              <UInput v-model="LoginState.password" type="password" />
               <!-- 綁定密碼 -->
             </UFormGroup>
 
@@ -220,8 +211,7 @@ const links = [
               <UButton
                 :loading="loginStatus === 'pending'"
                 type="submit"
-                @click="onSubmit"
-              >
+                @click="onSubmit">
                 登入
               </UButton>
             </div>
@@ -233,52 +223,27 @@ const links = [
             :schema="registerSchema"
             :state="RegisterState"
             class="space-y-4"
-            @submit="onRegister"
-          >
-            <UFormGroup
-              label="Name"
-              name="name"
-            >
+            @submit="onRegister">
+            <UFormGroup label="Name" name="name">
               <UInput v-model="RegisterState.name" />
               <!-- 綁定 email -->
             </UFormGroup>
-            <UFormGroup
-              label="Email"
-              name="email"
-            >
+            <UFormGroup label="Email" name="email">
               <UInput v-model="RegisterState.email" />
               <!-- 綁定 email -->
             </UFormGroup>
 
-            <UFormGroup
-              label="Password"
-              name="password"
-            >
-              <UInput
-                v-model="RegisterState.password"
-                type="password"
-              />
+            <UFormGroup label="Password" name="password">
+              <UInput v-model="RegisterState.password" type="password" />
               <!-- 綁定密碼 -->
             </UFormGroup>
 
-            <UFormGroup
-              label="Confirm Password"
-              name="confirmPassword"
-            >
-              <UInput
-                v-model="RegisterState.confirmPassword"
-                type="password"
-              />
+            <UFormGroup label="Confirm Password" name="confirmPassword">
+              <UInput v-model="RegisterState.confirmPassword" type="password" />
               <!-- 綁定確認密碼 -->
             </UFormGroup>
-            <UFormGroup
-              label="序號"
-              name="serialnumber"
-            >
-              <UInput
-                v-model="RegisterState.serialNumber"
-                type="text"
-              />
+            <UFormGroup label="序號" name="serialnumber">
+              <UInput v-model="RegisterState.serialNumber" type="text" />
 
               <!-- 綁定確認序號 -->
             </UFormGroup>
@@ -286,8 +251,7 @@ const links = [
               <UButton
                 class="bg-blue-500 text-white p-2 rounded-md"
                 :loading="registerStatus === 'pending'"
-                type="submit"
-              >
+                type="submit">
                 註冊
               </UButton>
             </div>
@@ -297,9 +261,7 @@ const links = [
       <!-- <h2>會員中心</h2> -->
     </div>
     <footer class="bg-gray-100 p-4 mt-16">
-      <div class="text-center">
-        Footer
-      </div>
+      <div class="text-center">Footer</div>
     </footer>
   </div>
 </template>
@@ -320,7 +282,7 @@ const links = [
   display: block; /* 確保按鈕是塊級元素 */
 }
 .group:hover::after {
-  content: '';
+  content: "";
   position: absolute;
   left: 50%;
   transform: translateX(-50%);
