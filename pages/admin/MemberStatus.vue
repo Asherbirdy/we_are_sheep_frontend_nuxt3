c9fc43ae03a01f8e721d632d7c176257aee4e938
WILL CHOU <will92068@gmail.com>
2024-11-30 20:00:29 +0800

    11.30

---
+++ pages/admin/MemberStatus.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+    <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio, asperiores distinctio voluptates commodi iusto odio incidunt dignissimos, quas repellat officiis beatae cum consectetur similique voluptatem aut nesciunt! Quia, fuga laboriosam?</h2>
+</template>
--- pages/admin/lifeBibleBlog.vue
+++ pages/admin/lifeBibleBlog.vue
@@ -7,6 +7,7 @@ definePageMeta({
 <template>
   <div>
     <h1>Life Bible Blog</h1>
+    <h2>55555555555555</h2>
   </div>
 </template>
 
