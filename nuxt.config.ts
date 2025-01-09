f1fc92a703ed26f8efdc1a850689f562c2db75d1
Asher Lien <birdym.tw@gmail.com>
2025-01-09 11:48:01 +0800

    更新

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,9 +28,9 @@ export default defineNuxtConfig({
   },
 
   experimental: {
-    payloadExtraction: true,
-    renderJsonPayloads: true, // 如果你的應用需要將 JSON 資料渲染到頁面上，則可以開啟此選項。這對於需要 SEO 的應用特別有用。
-    typedPages: true, // 如果你使用 TypeScript 並希望在頁面中獲得類型檢查，則可以開啟此選項。
+    payloadExtraction: false, // 關閉 payloadExtraction (SSG terminal 會報錯)
+    // renderJsonPayloads: true, // 如果你的應用需要將 JSON 資料渲染到頁面上，則可以開啟此選項。這對於需要 SEO 的應用特別有用。
+    // typedPages: true, // 如果你使用 TypeScript 並希望在頁面中獲得類型檢查，則可以開啟此選項。
   },
   modules: [
     '@vueuse/nuxt',
