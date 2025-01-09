2c964de620a99ea3e2fbdd9b5946044067ab4169
WILL CHOU <will92068@gmail.com>
2025-01-09 23:17:49 +0800

    1/9日期更新

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,63 @@
 <script setup lang="ts">
-import { useDayjs } from "#dayjs";
-import { useActivity } from "@/apis"; // 假設這是你的 API 模組
-import { onMounted, ref } from "vue";
+import { useDayjs } from '#dayjs'
+import { useActivity } from '@/apis' // 假設這是你的 API 模組
+import { onMounted, ref } from 'vue'
 
 // 使用 dayjs-nuxt 插件
-const dayjs = useDayjs();
+const dayjs = useDayjs()
 
 // 活動列表
 // const activityList = ref([]);
-const isLoading = ref(false); // 載入狀態
-const errorMessage = ref(""); // 錯誤訊息
+const isLoading = ref(false) // 載入狀態
+const errorMessage = ref('') // 錯誤訊息
 
 const { data: activityData, status } = await useActivity.showActivity({
-  year: dayjs().format("YYYY"), // 詢問為何是payload
-  month: dayjs().format("M"),
-});
+  year: dayjs().format('YYYY'), // 詢問為何是payload
+  month: dayjs().format('M'),
+})
+
+// 格式化日期方法
+const formatDate = (dateString) => {
+  const date = new Date(dateString)
+  return date.toISOString().split('T')[0]
+}
 </script>
 
 <template>
   <div class="container mx-auto mt-8 bg-white rounded-lg shadow">
     <!-- 載入中狀態 -->
-    <div v-if="isLoading" class="text-center py-4 text-[#98FB98]">資料載入中...</div>
+    <div
+      v-if="isLoading"
+      class="text-center py-4 text-[#98FB98]"
+    >
+      資料載入中...
+    </div>
 
     <!-- 錯誤訊息 -->
-    <div v-else-if="errorMessage" class="text-center text-[#98FB98] py-4">
+    <div
+      v-else-if="errorMessage"
+      class="text-center text-[#98FB98] py-4"
+    >
       {{ errorMessage }}
     </div>
 
     <!-- 活動列表 -->
     <div class="p-6">
-      <!-- {{ activityData }} -->
       <div class="space-y-4">
         <div
           v-for="(activity, index) in activityData?.data"
           :key="index"
-          class="flex justify-between items-center border-b pb-3">
-          <a href="#" class="text-black font-bold hover:underline">
+          class="flex justify-between items-center border-b pb-3"
+        >
+          <a
+            href="#"
+            class="text-black font-bold hover:underline"
+          >
             {{ activity.title }}
           </a>
-          <span class="text-black text-sm">{{ activity.date }}</span>
+          <span class="text-black text-sm">{{ formatDate(activity.date) }}</span>
         </div>
       </div>
-      <!-- <div class="text-center text-[#98FB98] py-4">目前沒有活動資料。</div> -->
     </div>
   </div>
 </template>
