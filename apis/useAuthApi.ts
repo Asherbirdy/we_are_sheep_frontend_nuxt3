6d911b8ec88f886e2d0ee48a81c2f2d57132e331
Asher Lien <birdym.tw@gmail.com>
2025-03-16 19:15:47 +0800

    done

--- apis/useAuthApi.ts
+++ apis/useAuthApi.ts
@@ -19,24 +19,17 @@ export const useAuthApi = {
       body: state,
       server: false,
       lazy: true,
-      immediate: false, // 阻止立即發出請求
-      watch: false, // 監聽一組響應式來源，並在它們發生變化時自動刷新所獲得的結果。
+      immediate: false,
+      watch: false,
     })
   },
   // 存會員登入狀態
   useStatus: async () => {
     return await useRequestApi('/users/showMe', {
       method: 'GET',
-      // body: state,
       server: false,
       lazy: true,
-      // immediate: false, // 阻止立即發出請求
-      watch: false, // 監聽一組響應式來源，並在它們發生變化時自動刷新所獲得的結果。
+      watch: false,
     })
   },
 }
-// https://nuxt.com.cn/docs/api/composables/use-fetch   --->  参数
-
-// 登入
-
-// postMan 註冊登入系統都寫在這
--- composables/useRequestApi.ts
+++ composables/useRequestApi.ts
@@ -6,10 +6,7 @@ export const useRequestApi = <DataT, ErrorT = any>(
 ) => {
   const config = useRuntimeConfig()
   const baseUrl = import.meta.env.DEV ? config.public.apiUrl : `${window.location.origin}/api/v1`
-  // 有處理.env
   const fetch = useNuxtApp().$Fetch
-  // baseUrl = http://84.10.7777.40:XXX/api/v9
-  // URL = /auth/login
   return useFetch<DataT, ErrorT>(`${baseUrl}${url}`, {
     ...options,
     $fetch: fetch,
